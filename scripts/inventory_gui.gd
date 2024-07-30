150caddf7ad6345a931c71baf7c1daa11e27ab03
Jose Ilcle <joseilclemaia@gmail.com>
2024-07-30 12:28:32 -0300

    criação de npc

--- scripts/inventory_gui.gd
+++ scripts/inventory_gui.gd
@@ -1,11 +1,12 @@
 extends Control
 
 var isOpen: bool = false
+
 @onready var inventory: Inventory = preload("res://invetory/player_inventory.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 func update():
-	for i in range(min(Inventory.item.size(), slots.size())):
-		slots[i].update(inventory.items[i])
+	for i in range(min(inventory.item.size(), slots.size())):
+		slots[i].update(inventory.item[i])
 func open():
 	visible = true
 	isOpen = true
