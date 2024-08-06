c7be754eeaac52488e256bfe8eeb49e04b9b369c
Jose Ilcle <joseilclemaia@gmail.com>
2024-08-06 19:50:03 -0300

    cenario 2 e menu atualizado

--- scenes/Next_area.gd
+++ scenes/Next_area.gd
@@ -3,13 +3,10 @@ extends Area2D
 var entered = false
 func _on_body_entered(body: CharacterBody2D):
 	entered = true
-	if entered == true:
-		_process(delta)
 	
 func _on_body_exited(body):
 	entered = false 
 	
 func _process(delta):
 	if entered == true:
-		if Input.is_action_just_pressed("Interact"):
-			get_tree().change_scene_to_file("res://scenes/outerscene_2.tscn")
+		get_tree().change_scene_to_file("res://scenes/outerscene_2.tscn")
